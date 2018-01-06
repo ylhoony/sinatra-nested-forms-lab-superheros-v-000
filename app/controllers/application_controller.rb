e42a1c4a0fc51f9c8cb5896bc898b35fdf7b7467
Hoon Lee <yl.hoony@gmail.com>
2018-01-06 21:46:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    
+  end
 
 end
